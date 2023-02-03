9e576fb43a7bdc212af9dc9ac8a2e57799c00da9
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-02-03 11:15:42 -0300

    Update alocação de matriz e preenchendo com indice.c

--- laboratorio estrutura 1/alocação de matriz e preenchendo com indice.c
+++ laboratorio estrutura 1/alocação de matriz e preenchendo com indice.c
@@ -1,31 +1,35 @@
 #include<stdio.h>
+
 #include<stdlib.h>
 
 int main(void){
+ 
  int linhas=3,colunas=3,linha,coluna;
+ 
  int **mindice= (int**) malloc(linhas * sizeof(int*));
+ 
  for(linha=0;linha<linhas;linha++){
      mindice[linha]=(int*) malloc(colunas*sizeof(int));
-
  }
+ 
  for (linha = 0; linha < linhas; linha++) {
+  
      for(coluna=0;coluna<colunas;coluna++){
          mindice[linha][coluna] = linha*3 + coluna;
      }
-
  }
+ 
  for (linha = 0; linha < linhas; linha++) {
+  
      for(coluna=0;coluna<colunas;coluna++){
          printf("%d\t",mindice[linha][coluna]);
      }
-
  }
+ 
  for(linha=0;linha<linhas;linha++){
          free(mindice[linha]);
      }
+ 
  free(mindice);
-
-
-
     return 0;
 }
