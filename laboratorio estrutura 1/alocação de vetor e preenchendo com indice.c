c0ff9eab141905be6001f153b93ee981e6bc6cc9
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-02-03 11:12:44 -0300

    Add files via upload

---
+++ laboratorio estrutura 1/alocação de vetor e preenchendo com indice.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int i;
+    int *vindice=(int*) malloc(10*sizeof(int));
+    if(vindice== NULL){
+        printf("memoria insuficiente");
+        exit(1);
+    }
+    else{
+    for ( i = 0; i < 10; i++){
+        vindice[i] = i;
+    }
+    for ( i = 0; i < 10; i++){
+       printf("o indice do %d elemento foi %d\n",i+1,vindice[i]);
+    }
+
+    free(vindice);
+    return(0);
+    }
+}
