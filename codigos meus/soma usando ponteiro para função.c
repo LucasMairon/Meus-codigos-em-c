95db58ef85b13e43f48ec5f3f8901e3c01318dff
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-02-03 20:53:08 -0300

    Create soma usando ponteiro para função.c

---
+++ codigos meus/soma usando ponteiro para função.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int soma(int a,int b){
+    return a+b;
+}
+
+int calcula(int x, int y, int(*operacao)(int,int)){
+    return(*operacao)(x,y);
+}
+
+int main(){
+
+int resultado=calcula(5,3,soma);
+
+printf("resultado:%d",resultado);
+
+return(0);
+
+}
