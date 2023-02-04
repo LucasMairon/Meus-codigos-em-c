d8c75a0252ba7aba8c17947c1052df441969f2b5
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-02-03 21:03:40 -0300

    Create ponteiro para função.c

---
+++ codigos meus/ponteiro para função.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+float div(float x, float y){
+  
+    return(x/y);
+}
+int main(){
+
+float(*pontdiv)(float,float);
+
+pontdiv=div;
+
+printf("%f",pontdiv(10,5));
+
+    return(0);
+}
