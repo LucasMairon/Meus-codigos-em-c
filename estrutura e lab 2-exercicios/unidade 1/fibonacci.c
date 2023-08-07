fb29234251f1f11ee6f50de7ba0e1df8d46ab7cf
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-08-07 12:50:58 +0000

    adicionando questao 2

--- estrutura e lab 2-exercicios/unidade 1/fibonacci.c
+++ estrutura e lab 2-exercicios/unidade 1/fibonacci.c
@@ -1,13 +1,24 @@
-#include<stdio.h>
-int fibonacci (int n ) {
-if ( n <= 1) {
-return n ;
-}
-else {
-return fibonacci ( n - 1) + fibonacci ( n - 2) ;
-}
+#include <stdio.h>
+int fibonacci(int n){
+    if (n <= 1){
+        return n;
+    }
+    else{
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
-int main () {
-fibonacci (15) ;
+int main(){
+
+    fibonacci(15);
+
 }
+
+
+//a) Depure o c´odigo para descobrir quantas vezes o fibonacci(5) ´e calculado (requisitado).
+// 88 vezes: para saber isso basta crear um breakpoint com condição(b fibonacci if(n==5))
+//b) Descubra atrav´es da depura¸c˜ao, qual a maior quantidade de n´ıveis (frames) esse c´odigo alcan¸ca.
+// 14 vezes: usa b fibonacci if n==1,da um finish e da um bt/where
+//Depure o c´odigo at´e o momento que o primeiro fibonacci(14) ´e calculado e o execut´avel ir´a come¸car
+//a calcular o fibonacci(13).
+// usar step para entrar nas funções, n para passar os comandos e display para acompanhar a variavel n
---
+++ estrutura e lab 2-exercicios/unidade 1/mdc.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+// Funcao para calcular algo
+int operacaoMatematica(int a, int b){
+    int r;
+
+    while (b != 0){
+        r = a % b;
+        a = b;
+        b = r;
+    }
+
+    return a;
+}
+
+int main(){
+    int num1, num2;
+
+    printf(" Digite o primeiro numero : ");
+    scanf("%d", &num1);
+
+    printf(" Digite o segundo numero : ");
+    scanf("%d", &num2);
+
+    int res = operacaoMatematica(num1, num2);
+
+    printf("A operacao de %d e %d e: %d\n", num1, num2, res);
+
+    return 0;
+}
+
+//a) Execute o algoritmo em modo de depura¸c˜ao.
+// usar breakpoint step e next
+//b) Descubra atrav´es da depura¸c˜ao, qual a maior profundidade de (frames) esse c´odigo alcan¸ca.
+//depende dos numeros que a pessoa colocar
+//c) Descubra o que ocorre quando um parˆametro ´e 0.
+// se um dos numeros for zero a função vai retornar o outro parametro, contanto que o outro seja diferente de 0
