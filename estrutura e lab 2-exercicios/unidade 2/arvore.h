a3a4c858894b8f25ad2355a0c93f9f22eb7094b5
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-08-31 13:40:57 -0300

    Update arvore.h

--- estrutura e lab 2-exercicios/unidade 2/arvore.h
+++ estrutura e lab 2-exercicios/unidade 2/arvore.h
@@ -2,4 +2,22 @@ typedef struct no No;
 
 No* insere_elemento(No* raiz,int dados);
 
-int altura(No* raiz);
+int alturaDaArvore(No* raiz);
+
+No* remove_elemento(No* raiz, int dados);
+
+void imprimeArvore(No* raiz);
+
+int alturaDoNo(No* raiz);
+
+int sucessor(No* raiz);
+
+int max(int a,int b);
+
+int balanceamento(No* raiz);
+
+int verificaBalanceamento(No* raiz);
+
+No* rotacaoDir(No* raiz);
+
+No* rotacaoEsq(No* raiz);
