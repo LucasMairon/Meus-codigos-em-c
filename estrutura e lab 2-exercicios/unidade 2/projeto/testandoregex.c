090b5f2d5889cc9b9580a667be82695c42c784a0
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-09-21 15:08:44 -0300

    atualizando funcoes regex

--- estrutura e lab 2-exercicios/unidade 2/projeto/testandoregex.c
+++ estrutura e lab 2-exercicios/unidade 2/projeto/testandoregex.c
@@ -1,37 +1,78 @@
 #include<regex.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+const short int FLAG = 1;
+const char NUMERO_PADRAO[] = "[0-9]"; 
+const char EMAIL_PADRAO[] = "([a-z0-9\.\-]{2,})@([a-z 0-9]{2,})(\.[a-z]{2,})(\.[a-z]{2,})?";
+const char TELEFONE_PADRAO[] = "([\(0-9\)]{2})([\s0-9\-]{4,5})([0-9]{4})";
+const char NOME_PADRAO[] = "[a-zA-Z]+";
+
+
+int isvalidPadrao(char* string,const char* PADRAO,const int FLAG){
+    regex_t regex;
+    int valor_regex = regcomp(&regex,PADRAO,FLAG);
+    
+    if(valor_regex == 0){
+        //valor_regex = regexec(&regex,string,0,NULL,FLAG);
+        valor_regex = regexec(&regex,string,0,(regmatch_t *)NULL,0);
+        regfree(&regex);
+        if(valor_regex == 0){
+            return 1;
+        }else{
+            return 0;
+        }
+    }else{
+        printf("erro na verificacao,tente novamente\n");
+        return 0;
+    }
+    
+}
+
+char* capturastring(char* string,const char* PADRAO,const int FLAG){
+
+   
+   int valor = isvalidPadrao(string,PADRAO,FLAG);
+   if(valor != 1){
+    do{
+    
+            printf("padrao invalido, tente novamente: ");
+            scanf(" %[^\n]",string);
+            valor = isvalidPadrao(string,PADRAO,FLAG);
+    
+    }while(valor != 1);
+
+   }
+   
+   return string;
+}
+
+
+int capturaNumero(char* numero,const char* PADRAO,const int FLAG){
+    int valor = isvalidPadrao(numero,PADRAO,FLAG);
+    
+    if(valor != 1){
+    do{
+   
+        printf("padrao invalido, tente novamente: ");
+        scanf(" %[^\n]",numero);
+        valor = isvalidPadrao(numero,PADRAO,FLAG);
+   
+   }while(valor != 1);
+
+   
+   }
+   return atoi(numero);
+   
+}
+
 
 int main(){
-    
-    regex_t regex;
-    //int valor;
-    int valor2;
+    char* verifica_nome = capturastring("(84) 998976167",TELEFONE_PADRAO,FLAG);
+    printf("%s\n",verifica_nome);
+    // int valor = capturaNumero("c",NUMERO_PADRAO,FLAG);
+    // printf("%d\n",valor);
 
-    //valor = regcomp(&regex,"([a-z0-9\.\-]{2,})@([a-z 0-9]{2,})(\.[a-z]{2,})(\.[a-z]{2,})?", 1);
-    //não ta funcionando :
-    //valor2 = regcomp(&regex,"([0-9]{2})\s([0-9]{4,5})\-([0-9]{4})",1);
-    valor2 = regcomp(&regex,"([0-9]{2})([0-9]{4,5})([0-9]{4})",1);
-  if (valor2 == 0) {
-        printf("RegEx compiled successfully.\n");
-    }
-    else {
-        printf("Compilation error.\n");
-    }
-
-  //char email[] = "lucAs@gmail.com";
-  char tel[] = "8499896167";
-   
-  valor2 = regexec(&regex,tel, 0, NULL, 1);
-
-  
-if (valor2 == 0) {
-        printf("Pattern found.\n");
-    }
-    else if (valor2 == REG_NOMATCH) {
-        printf("Pattern not found.\n");
-    }
-    else {
-        printf("An error occured.\n");
-    }
-    return 0;
 }
